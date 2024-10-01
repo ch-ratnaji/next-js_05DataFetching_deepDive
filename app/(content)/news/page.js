fbd210df71ba1a67ade1c4824ab6b29fc0e29d9b
ch-ratnaji <ratnaji.it91@gmail.com>
2024-10-01 17:34:17 +0530

    i removed Dummy_newsList and get the news from backend in news page.js , here in this repository i added backend with database

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,11 +1,44 @@
-import { DUMMY_NEWS } from "@/dummy-news";
+"use client";
+
+import { useEffect, useState } from "react";
 import NewsList from "@/components/news-list";
 
 export default function NewsPage() {
+  const [error, setError] = useState();
+  const [isLoading, setIsLoading] = useState();
+  const [news, setNews] = useState();
+
+  useEffect(() => {
+    async function fetchNews() {
+      setIsLoading(true);
+      const response = await fetch("http://localhost:8080/news");
+      if (!response.ok) {
+        setError("failed to fetch news");
+        setIsLoading(false);
+      }
+      const news = await response.json();
+      setIsLoading(false);
+      setNews(news);
+    }
+    fetchNews();
+  }, []);
+
+  if (isLoading === true) {
+    <p>Loading....</p>;
+  }
+
+  if (error) {
+    <p>{error}</p>;
+  }
+
+  let newsContent;
+  if (news) {
+    newsContent = <NewsList news={news} />;
+  }
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {newsContent}
     </>
   );
 }
