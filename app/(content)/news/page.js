6698b97dcbd0249d1bcd87dff42f817498d4f228
ch-ratnaji <ratnaji.it91@gmail.com>
2024-10-02 17:00:58 +0530

    In the news page.js i've removed useEffect and useState because when we use useClient if we check the source code we dont see any information related to news so i convert this back into server side component.Now for data fetching i used fetch directly in the componenet itself since next.js supports this feature so i removed all unnecessary code here.

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,44 +1,18 @@
-"use client";
-
-import { useEffect, useState } from "react";
 import NewsList from "@/components/news-list";
 
-export default function NewsPage() {
-  const [error, setError] = useState();
-  const [isLoading, setIsLoading] = useState();
-  const [news, setNews] = useState();
+export default async function NewsPage() {
+  const response = await fetch("http://localhost:8080/news");
 
-  useEffect(() => {
-    async function fetchNews() {
-      setIsLoading(true);
-      const response = await fetch("http://localhost:8080/news");
-      if (!response.ok) {
-        setError("failed to fetch news");
-        setIsLoading(false);
-      }
-      const news = await response.json();
-      setIsLoading(false);
-      setNews(news);
-    }
-    fetchNews();
-  }, []);
-
-  if (isLoading === true) {
-    <p>Loading....</p>;
+  if (!response.ok) {
+    throw new Error("Failed to fetch news.");
   }
 
-  if (error) {
-    <p>{error}</p>;
-  }
+  const news = await response.json();
 
-  let newsContent;
-  if (news) {
-    newsContent = <NewsList news={news} />;
-  }
   return (
     <>
       <h1>News Page</h1>
-      {newsContent}
+      <NewsList news={news} />
     </>
   );
 }
